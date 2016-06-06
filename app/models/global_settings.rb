280b26db75391c995be477e13a786555c82268c1
Meatballs <eat_meatballs@hotmail.co.uk>
2016-06-06 21:06:20 +0100

    Fix merge between fix_cmd_exec and redirect_all

--- app/models/global_settings.rb
+++ app/models/global_settings.rb
@@ -3,9 +3,6 @@ class GlobalSettings < ActiveRecord::Base
   attr_accessible :smtp_timeout, :asynchronous, :bing_api, :beef_url, :reports_refresh, :singleton
 
   validates :site_url, uri: true
-  validates :command_apache_restart, :presence => true, :length => {:maximum => 255}
-  validates :command_apache_vhosts, :presence => true, :length => {:maximum => 255}
-  validates :sites_enabled_path, :presence => true, :length => {:maximum => 255}
   validates :smtp_timeout, :presence => true, :length => {:maximum => 2},
             :numericality => {:greater_than_or_equal_to => 1, :less_than_or_equal_to => 20}
 
